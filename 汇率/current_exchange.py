09ecdba039797433113fe704f4e9cd4c9a8cfd7f
jialanxin <jia-lanxin@foxmail.com>
2018-06-25 20:03:44 +0800

    add itchat
    using xpath

--- 汇率/current_exchange.py
+++ 汇率/current_exchange.py
@@ -1,18 +1,20 @@
 import requests
-import re
+from lxml import etree
 import time
-from bs4 import BeautifulSoup
 import csv
+import itchat
 
-while True:
-	localtime = time.localtime(time.time())
-	now = time.asctime(localtime)
-	print(now)
-	r = requests.get('http://www.boc.cn/sourcedb/whpj/')
-	soup = BeautifulSoup(r.content.decode('utf-8'),features='lxml')
-	data = soup.body.contents[1].contents[13].contents[1].contents[-2].contents[-1].contents[-4].contents[7].string
-	f = open('dataset.csv','a')
-	writer = csv.writer(f)
-	writer.writerow([now,data])
-	f.close()
-	time.sleep(300)
+
+localtime = time.localtime(time.time())
+now = time.asctime(localtime)
+print(now)
+r = requests.get('http://www.boc.cn/sourcedb/whpj/')
+html = etree.HTML(r.content.decode('utf-8'))
+data = html.xpath('/html/body/div[1]/div[3]/div[1]/div[2]/table/tr[27]/td[5]//text()')
+itchat.auto_login(enableCmdQR=True)
+itchat.send(data[0], toUserName='filehelper')
+#f = open('dataset.csv','a')
+#writer = csv.writer(f)
+#writer.writerow([now,data])
+# f.close()
+#/body/div[1]/div[5]/div[1]/div[2]/table/tbody/tr[27]/td[5]
