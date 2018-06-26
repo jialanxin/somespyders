ad47e00f98f8ceb93d8b13d8e1e5b78e9b058100
jialanxin <jia-lanxin@foxmail.com>
2018-06-26 18:44:52 +0800

    initial version 0.1

--- 汇率/current_exchange.py
+++ 汇率/current_exchange.py
@@ -1,20 +1,18 @@
 import requests
 from lxml import etree
 import time
-import csv
-import itchat
 
 
-localtime = time.localtime(time.time())
-now = time.asctime(localtime)
-print(now)
-r = requests.get('http://www.boc.cn/sourcedb/whpj/')
-html = etree.HTML(r.content.decode('utf-8'))
-data = html.xpath('/html/body/div[1]/div[3]/div[1]/div[2]/table/tr[27]/td[5]//text()')
-itchat.auto_login(enableCmdQR=True)
-itchat.send(data[0], toUserName='filehelper')
-#f = open('dataset.csv','a')
-#writer = csv.writer(f)
-#writer.writerow([now,data])
-# f.close()
-#/body/div[1]/div[5]/div[1]/div[2]/table/tbody/tr[27]/td[5]
+while True:
+    localtime = time.localtime(time.time())
+    now = time.asctime(localtime)
+    print(now)
+    r = requests.get('http://www.boc.cn/sourcedb/whpj/')
+    html = etree.HTML(r.content.decode('utf-8'))
+    data = html.xpath('/html/body/div[1]/div[3]/div[1]/div[2]/table//text()')
+    try : 
+        position = data.index('美元')
+        print(data[position+6])
+        time.sleep(10)
+    except :
+        pass 
